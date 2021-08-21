f973bd511ad08166763ff37d63e56f05afb67527
Виталий Черков <vitaly.cherkov@gmail.com>
2021-08-21 13:19:11 +0300

    Доработано условие ДЗ1

--- src/shared/store/ApiStore/ApiStore.ts
+++ src/shared/store/ApiStore/ApiStore.ts
@@ -1,5 +1,12 @@
-import {IApiStore} from "./types";
+import {ApiResponse, IApiStore, RequestParams} from "./types";
 
 export default class ApiStore implements IApiStore {
-    // TODO: реализовать
+    constructor() {
+        // TODO: Примите из параметров конструктора baseUrl
+        // и присвойте его в this.baseUrl
+    }
+
+    request<SuccessT, ErrorT = any, ReqT = {}>(params: RequestParams<ReqT>): Promise<ApiResponse<SuccessT, ErrorT>> {
+        // TODO: Напишите здесь код, который с помощью fetch будет делать запрос
+    }
 }
